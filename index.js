71b5513caef79955a41dfce3f33b1b2d292ccdea
Jack Lu <jacklu@jacklu.me>
2019-03-02 23:55:38 +0800

    resq({+reference})

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import ReactJsonTree from "react-json-tree";
 import resq from "resq";
 import Memory from "resq/Access.Memory";
+import Delimited from 'resq/Reference.Delimited';
 import data from "resq/test/data";
 class Body extends React.Component {
 	constructor(props) {
 		super(props);
 		var access = Memory(data, { clone: true });
 		access.get = Delayed(access.get);
-		this.r = resq({ access });
+		this.r = resq({ access, reference: Delimited('.') });
 		this.state = {
 			input: 1,
 			schema: 'post',
